8620859f4df4414b5e695e0fd3e65332b5c72478
Ju.chao <cqjcrom@gmail.com>
2016-10-28 10:18:17 +0800

    initial code.

---
+++ spec/emp-app-view-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import EmpAppViewView from '../lib/emp-app-view-view';
+
+describe('EmpAppViewView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
